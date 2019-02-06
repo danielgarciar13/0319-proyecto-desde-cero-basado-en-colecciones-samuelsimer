9b9d0dd9dd4eade7e9c0731ae667194a9fc2d5ae
samuelremja <samuelremja@gmail.com>
2019-02-06 12:51:21 +0100

    commit 03

--- guitarra.java
+++ guitarra.java
@@ -6,49 +6,49 @@ public class guitarra
     private double precio;
     private String marca;
     private boolean esElectrica;
-    
 
     /**
      * Constructor for objects of class guitarra
      */
-    public guitarra()
+    public guitarra(int numeroIdentificativo, int numCuerdas, double elPrecio, String laMarca, boolean electrica)
     {
-        numeroCuerdas = 5;
-        precio = 0;
-        marca = "";
-        esElectrica = false;
+        int identificador = numeroIdentificativo;
+        numeroCuerdas = numCuerdas;
+        precio = elPrecio;
+        marca = laMarca;
+        esElectrica = electrica;
     }
-    
+
     public double getPrecio() {
         return precio;
     }
-    
+
     public void setPrecio(double elPrecio) {
         precio = elPrecio;
     }
-    
+
     public int getNumeroCuerdas() {
         return numeroCuerdas;
     }
-    
+
     public void setNumeroCuerdas(int numCuerdas) {
         numeroCuerdas = numCuerdas;
     }
-    
+
     public String getMarca() {
         return marca;
     }
-    
+
     public void setMarca(String laMarca) {
         marca = laMarca;
     }
-    
+
     public boolean getEsElectrica() {
         return esElectrica;
     }
-    
+
     public void setEsElectrica(boolean electrica) {
         esElectrica = electrica;
     }
-  
+
 }
