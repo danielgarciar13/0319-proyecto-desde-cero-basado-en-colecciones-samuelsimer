304d9d18040959b5eb2154ee52cd996248cb7f6f
samuelremja <samuelremja@gmail.com>
2019-02-06 12:25:11 +0100

    commit 01

---
+++ guitarra.java
@@ -0,0 +1,23 @@
+
+import java.util.ArrayList;
+public class guitarra
+{
+    private int numeroCuerdas;
+    private double precio;
+    private String marca;
+    private boolean esElectrica;
+    
+
+    /**
+     * Constructor for objects of class guitarra
+     */
+    public guitarra()
+    {
+        numeroCuerdas = 5;
+        precio = 0;
+        marca = "";
+        esElectrica = false;
+    }
+
+  
+}
